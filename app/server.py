88462a2f522d26a1694583a6f507f1dac0c0f76e
Martin Osial <marcinosiall@gmail.com>
2020-07-13 14:54:58 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://drive.google.com/uc?export=download&id=1ynbUKk6yFnGkKXjJKr9Z8ZgkMKx4h78y'
+model_file_url = 'https://drive.google.com/uc?export=download&id=1UqracWaZWlPXU-llL3zbrxcmLFYV4839'
 model_file_name = 'model'
 classes = ['white', 'black']
 path = Path(__file__).parent
